7eb4129afae2b039537e082544a6fabad905f7d5
Till Berger <till.berger@googlemail.com>
2013-04-18 23:19:14 +0200

    bla

---
+++ src/Card.java
@@ -0,0 +1,129 @@
+
+
+public class Card implements Comparable<Card> {
+	private String name, clan, condition, effect, rarity, elo;
+	private int lvl, pow, dmg, effectvalue, min, maxlvl, id;
+	/**
+	 * @param name
+	 * @param clan
+	 * @param condition
+	 * @param effect
+	 * @param rarity
+	 * @param lvl
+	 * @param pow
+	 * @param dmg
+	 * @param effectvalue
+	 * @param min
+	 * @param maxlvl
+	 * @param id
+	 */
+	public Card(String name, String clan, int lvl, int pow, int dmg,
+			String condition, String effect, int effectvalue, int min,
+			String elo, int maxlvl, int id, String rarity) {
+		super();
+		this.name = name;
+		this.clan = clan;
+		this.condition = condition;
+		this.effect = effect;
+		this.rarity = rarity;
+		this.lvl = lvl;
+		this.pow = pow;
+		this.dmg = dmg;
+		this.effectvalue = effectvalue;
+		this.min = min;
+		this.maxlvl = maxlvl;
+		this.id = id;
+		this.elo= elo;
+	}
+	
+	
+	
+	public String getElo() {
+		return elo;
+	}
+	public void setElo(String elo) {
+		this.elo = elo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getClan() {
+		return clan;
+	}
+	public void setClan(String clan) {
+		this.clan = clan;
+	}
+	public String getCondition() {
+		return condition;
+	}
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
+	public String getEffect() {
+		return effect;
+	}
+	public void setEffect(String effect) {
+		this.effect = effect;
+	}
+	public String getRarity() {
+		return rarity;
+	}
+	public void setRarity(String rarity) {
+		this.rarity = rarity;
+	}
+	public int getLvl() {
+		return lvl;
+	}
+	public void setLvl(int lvl) {
+		this.lvl = lvl;
+	}
+	public int getPow() {
+		return pow;
+	}
+	public void setPow(int pow) {
+		this.pow = pow;
+	}
+	public int getDmg() {
+		return dmg;
+	}
+	public void setDmg(int dmg) {
+		this.dmg = dmg;
+	}
+	public int getEffectvalue() {
+		return effectvalue;
+	}
+	public void setEffectvalue(int effectvalue) {
+		this.effectvalue = effectvalue;
+	}
+	public int getMin() {
+		return min;
+	}
+	public void setMin(int min) {
+		this.min = min;
+	}
+	public int getMaxlvl() {
+		return maxlvl;
+	}
+	public void setMaxlvl(int maxlvl) {
+		this.maxlvl = maxlvl;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+
+
+
+	@Override
+	public int compareTo(Card c) {
+		return name.compareTo(((Card)c).getName());
+	}
+}
+
---
+++ src/CardDB.java
@@ -0,0 +1,157 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class CardDB {
+	public final static int initialListSize=1000;
+	private ArrayList<Card> cards;
+	
+	public CardDB(){
+		cards= new ArrayList<Card>(initialListSize);
+		fromFileTest();
+	}
+	
+	public Card getCard(String name){
+		int i= getCardIndex(name);
+		if(i>=0){
+			return cards.get(i);
+		}else{
+			return null;
+		}
+	}
+	public Card getCard(int index){
+		if(cards==null) return null;
+		if(index>=0 && index<cards.size())
+			return cards.get(index);
+		else
+			return null;
+	}
+	public Card removeCard(String name){
+		int i= getCardIndex(name);
+		if(i<0)
+			return null;
+		else
+			return cards.remove(i);
+	}
+	public Card removeCard(int index){
+		if(cards==null) return null;
+		if(index>=0 && index<cards.size())
+			return cards.remove(index);
+		else
+			return null;
+	}
+	public int getCardIndex(String name){
+		if(cards==null || cards.isEmpty()) return -1;
+		int rechts= cards.size()-1;
+		int links=0;
+		do{
+			int mitte=(links+rechts)/2;
+			int c= name.compareTo(cards.get(mitte).getName());
+			if(c==0) return mitte;
+			if(c>0)
+				links=mitte+1;
+			else
+				rechts=mitte-1;
+		}while(links<=rechts);
+		
+		return -1;
+	}
+	
+	
+	
+	public void fromFileTest(){
+		if(cards==null) return;
+		cards.clear();
+		
+		String path = "C:\\Users\\Till Berger\\Desktop\\DBcardsSmall.txt";
+		String line;
+		System.out.println("Trying to read Cards");
+		try {
+			FileReader fr= new FileReader(path);
+			BufferedReader br  = new BufferedReader(fr);
+			
+			
+			
+			while((line=br.readLine())!=null){
+				//if line is a comment ignore
+				if(line.startsWith("//")) continue;
+				//if line is empty ignore
+				if(line.isEmpty()) continue;
+		
+				String[] sa = line.split(",");
+				//only if the split has lenght 14
+				if(sa.length==14){
+					
+					//only max level cards should be added:
+					if(sa[2].trim().compareTo(sa[11].trim())!=0) continue;
+					
+					Card c= new Card(/*Name*/sa[0].trim(),
+							/*Clan*/sa[1].trim(),
+							/*Level*/Integer.parseInt(sa[2].trim()),
+							/*Pow*/Integer.parseInt(sa[3].trim()),
+							/*Dmg*/Integer.parseInt(sa[4].trim()),
+							/*Condition*/sa[5].trim(),
+							/*Effect*/ sa[6].trim(),
+							/*Effectvalue*/Integer.parseInt(sa[7].trim()),
+							/*Min*/Integer.parseInt(sa[8].trim()),
+							/*Someting*/ sa[9].trim(),
+							/*Maxlevel*/Integer.parseInt(sa[11].trim()),
+							/*ID*/Integer.parseInt(sa[12].trim()),
+							/*Rarity*/ sa[13].trim()
+							);
+					cards.add(c);
+				}
+			}
+			fr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		
+		System.out.println("All cards read! Found "+ cards.size()+" suitable cards.");
+		System.out.print("Now sorting...");
+		Collections.sort(cards);
+		System.out.println("completed.");
+		System.out.print("Writing to file...");
+		try {
+			FileWriter fw = new FileWriter("C:\\Users\\Till Berger\\Desktop\\CardList2.txt");
+			BufferedWriter bw= new BufferedWriter(fw);
+			
+			
+			for(int i=0; i<cards.size(); i++){
+				Card c= cards.get(i);
+				String l= c.getName() +", "+ c.getClan() +", "+ c.getLvl() +", "+
+				c.getPow()+", "+c.getDmg()+", "+c.getCondition()+", "+c.getEffect()+", "+
+				c.getEffectvalue()+", "+c.getMin()+", "+c.getElo()+", "+
+				c.getMaxlvl()+", "+c.getId();
+				bw.write(l);
+				bw.newLine();
+			}
+			
+			
+			bw.flush();
+			fw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		System.out.println("completed.");
+	}
+	
+	
+	
+	public static void main(String[] args){
+		CardDB db= new CardDB();
+	}
+
+}
