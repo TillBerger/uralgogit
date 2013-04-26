cbdcdd2f7bfec6b57cd8c7e93f62f1153af44ab4
Botchi <Botchi@192.168.2.100>
2013-04-26 13:46:03 +0200

    haha

---
+++ src/Simulation.java
@@ -0,0 +1,75 @@
+
+public class Simulation {
+
+	public int life_p1;
+	public int pills_p1;
+	public int poison_value_p1;
+	public int poison_min_p1;
+	public int heal_value_p1;
+	public int heal_max_p1;
+	public boolean[] available_cards_p1;
+	public Card[] deck_p1;
+	
+	public int life_p2;
+	public int pills_p2;
+	public int poison_value_p2;
+	public int poison_min_p2;
+	public int heal_value_p2;
+	public int heal_max_p2;
+	public boolean[] available_cards_p2;
+	public Card[] deck_p2;
+	
+	public Simulation(int life_p1, int pills_p1, int life_p2, int pills_p2, Card[] deck_p1, Card[] deck_p2){
+	    this.life_p1			= life_p1;
+	    this.pills_p1			= pills_p1;
+	    this.life_p2			= life_p2;
+	    this.pills_p2			= pills_p2;
+	    this.deck_p1			= deck_p1;
+	    this.deck_p2			= deck_p2;
+	    
+	    this.poison_value_p1	= 0;
+	    this.poison_min_p1		= 0;
+	    this.heal_value_p1		= 0;
+	    this.heal_max_p1		= 0;
+	    this.available_cards_p1	= {true, true, true, true};
+	    this.poison_value_p2	= 0;
+	    this.poison_min_p2		= 0;
+	    this.heal_value_p2		= 0;
+	    this.heal_max_p2		= 0;
+	    this.available_cards_p2	= {true, true, true, true};
+	}
+
+	public Simulation(int life_p1, int pills_p1, int poison_value_p1, int poison_min_p1, int heal_value_p1, int heal_max_p1, boolean[] available_cards_p1, int life_p2, int pills_p2, int poison_value_p2, int poison_min_p2, int heal_value_p2, int heal_max_p2, boolean[] available_cards_p2, Card[] deck_p1, Card[] deck_p2){
+
+	    this.life_p1			= life_p1;
+	    this.pills_p1			= pills_p1;
+	    this.life_p2			= life_p2;
+	    this.pills_p2			= pills_p2;
+	    this.deck_p1			= deck_p1;
+	    this.deck_p2			= deck_p2;
+	    
+	    this.poison_value_p1	= poison_value_p1;
+	    this.poison_min_p1		= poison_min_p1;
+	    this.heal_value_p1		= heal_value_p1;
+	    this.heal_max_p1		= heal_max_p1;
+	    this.available_cards_p1	= available_cards_p1;
+	    this.poison_value_p2	= poison_value_p2;
+	    this.poison_min_p2		= poison_min_p2;
+	    this.heal_value_p2		= heal_value_p2;
+	    this.heal_max_p2		= heal_max_p2;
+	    this.available_cards_p2	= available_cards_p2;
+	}
+
+	public Simulation copy(){
+
+	    return new Simulation(life_p1, pills_p1, poison_value_p1, poison_min_p1, heal_value_p1, heal_max_p1, available_cards_p1.clone(), life_p2, pills_p2, poison_value_p2, poison_min_p2, heal_value_p2, heal_max_p2, available_cards_p2.clone(), deck_p1, deck_p2);
+
+	}
+
+
+	public void calculateFight(){
+
+
+
+	}
+}
