225e72556e428a3b273ca4475250fc5320c1fd6c
Botchi <Botchi@192.168.2.100>
2013-04-27 01:54:05 +0200

    d

--- src/com/Simulation.java
+++ src/com/Simulation.java
@@ -3,7 +3,6 @@ import java.util.Arrays;
 
 
 public class Simulation {
-	
 	public int life_p1;
 	public int pills_p1;
 	public int poison_value_p1;
@@ -55,7 +54,6 @@ public class Simulation {
 	}
 
 	public Simulation(int life_p1, int pills_p1, int poison_value_p1, int poison_min_p1, int heal_value_p1, int heal_max_p1, String teameffect_p1, boolean[] available_cards_p1, boolean[] bonus_p1, int life_p2, int pills_p2, int poison_value_p2, int poison_min_p2, int heal_value_p2, int heal_max_p2, String teameffect_p2, boolean[] available_cards_p2, boolean[] bonus_p2, Card[] deck_p1, Card[] deck_p2){
-
 	    this.life_p1			= life_p1;
 	    this.pills_p1			= pills_p1;
 	    this.life_p2			= life_p2;
@@ -78,36 +76,20 @@ public class Simulation {
 	}
 
 	public Simulation copy(){
-
 	    return new Simulation(life_p1, pills_p1, poison_value_p1, poison_min_p1, heal_value_p1, heal_max_p1, teameffect_p1, available_cards_p1.clone(), bonus_p1.clone(), life_p2, pills_p2, poison_value_p2, poison_min_p2, heal_value_p2, heal_max_p2, teameffect_p2, available_cards_p2.clone(), bonus_p2.clone(), deck_p1, deck_p2);
-
 	}
 
 	public boolean[] clanbonus(Card[] deck){
-		
 		boolean[] bonus = new boolean[4];
-		
-		for (int i=1; i<4; i++){
-			if (deck[0].getClan().equals(deck[i].getClan())){
-				bonus[0] = true;
-				bonus[i] = true;
+		for (int j=0; j<3; j++){
+			for (int i=j+1; i<4; i++){
+				if (deck[j].getClan().equals(deck[i].getClan())){
+					bonus[j] = true;
+					bonus[i] = true;
+				}
 			}
 		}
-		
-		for (int i=2; i<4; i++){
-			if (deck[1].getClan().equals(deck[i].getClan())){
-				bonus[1] = true;
-				bonus[i] = true;
-			}
-		}
-		
-		if (deck[2].getClan().equals(deck[3].getClan())){
-			bonus[2] = true;
-			bonus[3] = true;
-		}
-		
 		return bonus;
-		
 	}
 
 	public void calculateFight(){
