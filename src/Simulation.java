1138b63605144b5d2345965a0a094c01c4ad4f66
Botchi <Botchi@192.168.2.100>
2013-04-26 13:54:51 +0200

    haha2

--- src/Simulation.java
+++ src/Simulation.java
@@ -7,6 +7,7 @@ public class Simulation {
 	public int poison_min_p1;
 	public int heal_value_p1;
 	public int heal_max_p1;
+	public String teameffect_p1;
 	public boolean[] available_cards_p1;
 	public Card[] deck_p1;
 	
@@ -16,6 +17,7 @@ public class Simulation {
 	public int poison_min_p2;
 	public int heal_value_p2;
 	public int heal_max_p2;
+	public String teameffect_p2;
 	public boolean[] available_cards_p2;
 	public Card[] deck_p2;
 	
@@ -31,15 +33,17 @@ public class Simulation {
 	    this.poison_min_p1		= 0;
 	    this.heal_value_p1		= 0;
 	    this.heal_max_p1		= 0;
+	    this.teameffect_p1		= "";
 	    this.available_cards_p1	= {true, true, true, true};
 	    this.poison_value_p2	= 0;
 	    this.poison_min_p2		= 0;
 	    this.heal_value_p2		= 0;
 	    this.heal_max_p2		= 0;
+	    this.teameffect_p2		= "";
 	    this.available_cards_p2	= {true, true, true, true};
 	}
 
-	public Simulation(int life_p1, int pills_p1, int poison_value_p1, int poison_min_p1, int heal_value_p1, int heal_max_p1, boolean[] available_cards_p1, int life_p2, int pills_p2, int poison_value_p2, int poison_min_p2, int heal_value_p2, int heal_max_p2, boolean[] available_cards_p2, Card[] deck_p1, Card[] deck_p2){
+	public Simulation(int life_p1, int pills_p1, int poison_value_p1, int poison_min_p1, int heal_value_p1, int heal_max_p1, String teameffect_p1, boolean[] available_cards_p1, int life_p2, int pills_p2, int poison_value_p2, int poison_min_p2, int heal_value_p2, int heal_max_p2, String teameffect_p2, boolean[] available_cards_p2, Card[] deck_p1, Card[] deck_p2){
 
 	    this.life_p1			= life_p1;
 	    this.pills_p1			= pills_p1;
@@ -52,17 +56,19 @@ public class Simulation {
 	    this.poison_min_p1		= poison_min_p1;
 	    this.heal_value_p1		= heal_value_p1;
 	    this.heal_max_p1		= heal_max_p1;
+	    this.teameffect_p1		= teameffect_p1;
 	    this.available_cards_p1	= available_cards_p1;
 	    this.poison_value_p2	= poison_value_p2;
 	    this.poison_min_p2		= poison_min_p2;
 	    this.heal_value_p2		= heal_value_p2;
 	    this.heal_max_p2		= heal_max_p2;
+	    this.teameffect_p2		= teameffect_p2;
 	    this.available_cards_p2	= available_cards_p2;
 	}
 
 	public Simulation copy(){
 
-	    return new Simulation(life_p1, pills_p1, poison_value_p1, poison_min_p1, heal_value_p1, heal_max_p1, available_cards_p1.clone(), life_p2, pills_p2, poison_value_p2, poison_min_p2, heal_value_p2, heal_max_p2, available_cards_p2.clone(), deck_p1, deck_p2);
+	    return new Simulation(life_p1, pills_p1, poison_value_p1, poison_min_p1, heal_value_p1, heal_max_p1, teameffect_p1, available_cards_p1.clone(), life_p2, pills_p2, poison_value_p2, poison_min_p2, heal_value_p2, heal_max_p2, teameffect_p2, available_cards_p2.clone(), deck_p1, deck_p2);
 
 	}
 
